648b6881af316449b4207a684807a41e8671cf1b
slabitskiy <slabitskiy.igor@yandex.ua>
2016-12-04 22:39:32 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -18,4 +18,33 @@
 		nextButton: '.swiper-button-next',
 		prevButton: '.swiper-button-prev'
 	});
+
+	$('#menu-title').click( function () {
+		$(this).parent().parent().parent().hide();
+	});
+
+	$('#mobile-menu').click ( function () {
+		$('.overlay').show();
+	});
+
+    $('.navigation__item').click( function (e) {
+    	e.stopPropagation();
+        if ($(this).hasClass('active')) {
+            $('.navigation__item').removeClass('active');
+            $('.navigation__panel').hide();
+
+        } else {
+            $('.navigation__item').removeClass('active');
+            $(this).addClass('active');
+            $('.navigation__panel').hide();
+            var target = $(this).data('target');
+            $(target).show();
+        }
+    });
+	$(document).on('click', function(e){
+         if(!$(event.target).closest('.navigation__panel').length ) {
+         	$('.navigation__panel').hide();
+         	$('.navigation__item').removeClass('active')
+         }
+    });
 }());
